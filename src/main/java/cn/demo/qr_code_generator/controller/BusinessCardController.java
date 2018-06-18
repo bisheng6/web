88a4279cf22d4b92d1d3ff1f7e70a5756b7584de
dell-pc <565267339@qq.com>
2018-06-18 20:12:54 +0800

    add businesscard function

---
+++ src/main/java/cn/demo/qr_code_generator/controller/BusinessCardController.java
@@ -0,0 +1,26 @@
+package cn.demo.qr_code_generator.controller;
+
+import cn.demo.qr_code_generator.bean.BusinessCard;
+import cn.demo.qr_code_generator.service.BusinessCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BusinessCardController
+{
+    @Autowired
+    private BusinessCardService businessCardService;
+
+    @RequestMapping(value = "/businesscard", method = RequestMethod.GET)
+    public String getBusinessCardById(Model model, int id)
+    {
+        System.out.println(id);
+        BusinessCard businessCard = businessCardService.getBusinessCard(id);
+        model.addAttribute(businessCard);
+        // TODO: 2018/6/18 返回到一个页面 
+        return null;
+    }
+}
